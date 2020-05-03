ce53fe7dc2096ab44fc322087b242f3bd93f0fa7
wojnillowicz <64035391+wojnillowicz@users.noreply.github.com>
2020-05-03 22:19:10 +0200

    Add files via upload

---
+++ flagi.js
@@ -0,0 +1,93 @@
+var europeFlags = {Albania:'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Flag_of_Albania.svg/270px-Flag_of_Albania.svg.png',
+Andora:'https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Flag_of_Andorra.svg/270px-Flag_of_Andorra.svg.png',
+Austria:'https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Flag_of_Austria.svg/270px-Flag_of_Austria.svg.png',
+Bialorus: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Flag_of_Belarus.svg/270px-Flag_of_Belarus.svg.png',
+Belgia: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Flag_of_Belgium_%28civil%29.svg/270px-Flag_of_Belgium_%28civil%29.svg.png',
+Bulgaria: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Bulgaria.svg/270px-Flag_of_Bulgaria.svg.png',
+Chorwacja: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Flag_of_Croatia.svg/270px-Flag_of_Croatia.svg.png',
+Estonia: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Flag_of_Estonia.svg/188px-Flag_of_Estonia.svg.png',
+Finlandia: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_Finland.svg/188px-Flag_of_Finland.svg.png',
+Francja: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/188px-Flag_of_France.svg.png',
+Gruzja: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Flag_of_Georgia.svg/188px-Flag_of_Georgia.svg.png',
+Niemcy: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/188px-Flag_of_Germany.svg.png',
+Grecja: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Greece.svg/188px-Flag_of_Greece.svg.png',
+Wegry: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Flag_of_Hungary.svg/188px-Flag_of_Hungary.svg.png',
+Islandia: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Iceland.svg/188px-Flag_of_Iceland.svg.png',
+Irlandia: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Flag_of_Ireland.svg/188px-Flag_of_Ireland.svg.png',
+Wlochy: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Flag_of_Italy.svg/188px-Flag_of_Italy.svg.png',
+Kosowo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Flag_of_Kosovo.svg/188px-Flag_of_Kosovo.svg.png',
+Lotwa: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Flag_of_Latvia.svg/188px-Flag_of_Latvia.svg.png',
+Liechtenstein:'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Flag_of_Liechtenstein.svg/188px-Flag_of_Liechtenstein.svg.png',
+Litwa:'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Flag_of_Lithuania.svg/188px-Flag_of_Lithuania.svg.png',
+Luksemburg:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Flag_of_Luxembourg.svg/188px-Flag_of_Luxembourg.svg.png',
+Malta: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flag_of_Malta.svg/188px-Flag_of_Malta.svg.png',
+Moldawia: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Flag_of_Moldova.svg/188px-Flag_of_Moldova.svg.png',
+Monako: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Flag_of_Monaco.svg/188px-Flag_of_Monaco.svg.png',
+Czarnogora:'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Flag_of_Montenegro.svg/188px-Flag_of_Montenegro.svg.png',
+Holandia:'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_the_Netherlands.svg/188px-Flag_of_the_Netherlands.svg.png',
+Macedonia: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Flag_of_North_Macedonia.svg/188px-Flag_of_North_Macedonia.svg.png',
+Norway: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Norway.svg/188px-Flag_of_Norway.svg.png',
+Polska: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Flag_of_Poland.svg/188px-Flag_of_Poland.svg.png',
+Portugalia: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/188px-Flag_of_Portugal.svg.png',
+Rumunia: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flag_of_Romania.svg/188px-Flag_of_Romania.svg.png',
+Rosja: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f3/Flag_of_Russia.svg/188px-Flag_of_Russia.svg.png',
+Serbia: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Flag_of_Serbia.svg/188px-Flag_of_Serbia.svg.png',
+Slowacja: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Flag_of_Slovakia.svg/188px-Flag_of_Slovakia.svg.png',
+Slowenia: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Flag_of_Slovenia.svg/188px-Flag_of_Slovenia.svg.png',
+Hiszpania:  'https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Flag_of_Spain.svg/188px-Flag_of_Spain.svg.png',
+Szwecja: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Flag_of_Sweden.svg/188px-Flag_of_Sweden.svg.png',
+Szwajcaria: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Flag_of_Switzerland_%28Pantone%29.svg/143px-Flag_of_Switzerland_%28Pantone%29.svg.png',
+Turcja: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/1280px-Flag_of_Turkey.svg.png',
+Ukraina: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Flag_of_Ukraine.svg/188px-Flag_of_Ukraine.svg.png',
+}
+
+var europeCountries = ['Albania', 'Andora', 'Austria', 'Bialorus', 'Belgia', 'Bulgaria', 'Chorwacja', 'Estonia', 'Finlandia', 'Francja', 'Gruzja', 'Niemcy', 'Grecja',
+'Wegry', 'Islandia', 'Irlandia', 'Wlochy', 'Kosowo', 'Lotwa', 'Liechtenstein', 'Litwa' , 'Luksemburg', 'Malta', 'Moldawia', 'Monako', 'Czarnogora', 'Holandia',
+'Macedonia', 'Norwegia', 'Polska', 'Portugalia', 'Rumunia', 'Rosja', 'Serbia', 'Slowacja', 'Slowenia', 'Hiszpania', 'Szwecja', 'Szwajcaria', 'Turcja', 'Ukraina']
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+// expected output: 0, 1 or 2
+
+var chosenFlag = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Flag_of_Poland.svg/360px-Flag_of_Poland.svg.png";
+var correctAnswer = "Polska";
+var tot = 0;
+var count = 0;
+
+
+function capitalizeFirstLetter(string) {
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+function fn1(){
+  var userAnswer = document.getElementById('guess').value;
+  document.getElementById('showingUserAnswer').innerHTML = userAnswer;
+  document.getElementById('showingCorrectAnswer').innerHTML = correctAnswer;
+  document.getElementById('miniflag').src = chosenFlag
+  userAnswer = capitalizeFirstLetter(userAnswer);
+  if (userAnswer == correctAnswer) {
+    document.getElementById('correctOrNot').innerHTML = "Dobrze!";
+    tot = tot + 1;
+    document.getElementById('totalPoints').innerHTML = tot;
+  }else {
+    document.getElementById('correctOrNot').innerHTML = "Źle...";
+  }
+  //changing the site for a new try
+  document.getElementById('guess').value = "";
+  count = count + 1
+  var av = tot/count*100
+
+  document.getElementById('average').innerHTML = av.toFixed(0);
+  var randomIndex = getRandomInt(40);
+  console.log(randomIndex);
+  var randomCountry = europeCountries[randomIndex];
+  console.log(randomCountry);
+  console.log(europeFlags[randomCountry]);
+  chosenFlag = europeFlags[randomCountry];
+  correctAnswer = randomCountry;
+  // document.getElementById('country').innerHTML = chosenCountry;
+  document.getElementById("flag").src = chosenFlag;
+
+}
